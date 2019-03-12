e3e1cf22eb6d7daa9e37eed4c185143a9dc1e0d0
= <=>
2019-03-12 23:44:01 +0400

    Add point write realisation

--- app/src/main/java/com/simbirsoft/telegramconcurs/diagram/DiagramView.kt
+++ app/src/main/java/com/simbirsoft/telegramconcurs/diagram/DiagramView.kt
@@ -2,6 +2,8 @@ package com.simbirsoft.telegramconcurs.diagram
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 import com.simbirsoft.telegramconcurs.diagram.diagram_data.LineDiagramData
@@ -15,14 +17,22 @@ class DiagramView @JvmOverloads constructor(
 
     var yMax: Float = 0f
     var yMin: Float = 0f
+    var yRange: Float = 0f
 
     var xMax: Float = 0f
     var xMin: Float = 0f
+    var xRange: Float = 0f
 
-    var graphScaleY: Float = 1f
+    var defaultPaint = Paint()
+
+    var graphicsScaleX: Float = 1f
     private var _data: LineDiagramData? = null
 
     var position: Float = 0f
+
+    init {
+        defaultPaint.color = Color.RED
+    }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
@@ -31,10 +41,11 @@ class DiagramView @JvmOverloads constructor(
     }
 
     override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-        _data?.let {
-            for (item in it.values) {
-
+        canvas?.let {
+            _data?.let {
+                for (item in it.values) {
+                    drawPoint(translateX(item.x), translateY(item.y), canvas)
+                }
             }
         }
         postInvalidateOnAnimation()
@@ -43,22 +54,28 @@ class DiagramView @JvmOverloads constructor(
     fun setData(data:LineDiagramData) {
         yMax = data.maximumValue
         yMin = data.minimumValue
+        yRange = yMax - yMin
 
         xMin = data.values.first().x
         xMax = data.values.last().x
+        xRange = xMax - xMin
 
         _data = data
         invalidate()
     }
 
-    private fun translateX(x: Float) {
+    private fun translateX(x: Float) =
+            paddingLeft + ((x - xMin)/xRange * xSize)
 
-    }
-
-    private fun translateY(y: Float) = y
+    private fun translateY(y: Float) =
+            paddingTop + (ySize - ((y - yMin)/yRange) * ySize)
 
     fun setYScale(scale: Float) {
-        graphScaleY = scale
+        graphicsScaleX = scale
         invalidate()
+    }
+
+    fun drawPoint(x: Float, y: Float, canvas: Canvas) {
+        canvas.drawCircle(x,y,2f, defaultPaint)
     }
 }
